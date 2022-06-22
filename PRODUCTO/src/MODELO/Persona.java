e2b56e05344cccdc140fc577281978a93bc23a45
PaolaCriolloGranda <priscila.criollo.est@tecazuay.edu.ec>
2022-06-22 14:38:34 -0500

    Actualizacion clase persona y creacion clase Usuario

--- PRODUCTO/src/MODELO/Persona.java
+++ PRODUCTO/src/MODELO/Persona.java
@@ -17,6 +17,24 @@ public class Persona {
     private String direccion;
     private String telefono;
     private String fecha_nac;
+    private String usuario;
+    private String contrasenia;
+    private int codigo_usuario;
+    private int codigo_cliente;
+
+    public Persona(String cedula, String nombre, String apellido, String correo, String direccion, String telefono, String fecha_nac, String usuario, String contrasenia, int codigo_usuario, int codigo_cliente) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fecha_nac = fecha_nac;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.codigo_usuario = codigo_usuario;
+        this.codigo_cliente = codigo_cliente;
+    }
 
     public String getCedula() {
         return cedula;
@@ -73,7 +91,39 @@ public class Persona {
     public void setFecha_nac(String fecha_nac) {
         this.fecha_nac = fecha_nac;
     }
-    
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public int getCodigo_usuario() {
+        return codigo_usuario;
+    }
+
+    public void setCodigo_usuario(int codigo_usuario) {
+        this.codigo_usuario = codigo_usuario;
+    }
+
+    public int getCodigo_cliente() {
+        return codigo_cliente;
+    }
+
+    public void setCodigo_cliente(int codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+
     
     
 
---
+++ PRODUCTO/src/MODELO/Usuario.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODELO;
+
+/**
+ *
+ * @author prisc
+ */
+public class Usuario {
+     private String cedula;
+    private int codigo_cliente;
+    private String usuario;
+    private String contrasenia;
+    private int codigo_usuario;
+    private String nombre;
+
+    public Usuario(String cedula, int codigo_cliente, String usuario, String contrasenia, int codigo_usuario, String nombre) {
+        this.cedula = cedula;
+        this.codigo_cliente = codigo_cliente;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.codigo_usuario = codigo_usuario;
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getCodigo_cliente() {
+        return codigo_cliente;
+    }
+
+    public void setCodigo_cliente(int codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public int getCodigo_usuario() {
+        return codigo_usuario;
+    }
+
+    public void setCodigo_usuario(int codigo_usuario) {
+        this.codigo_usuario = codigo_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
